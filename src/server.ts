eaef308533b97374b7fd4080925fc372f5393d55
Eduardo Costa <dev.eduardo.lc@gmail.com>
2026-02-28 22:33:37 -0300

    add session_id from cookies and middleware to check session id

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
+
 import { knex } from "./database";
 import { env } from "./env";
-import { transactionsRoutes } from "./routes/transactions";
+import { transactionsRoutes } from "./routes/transactions.routes";
 
 const app = fastify();
+
+app.register(cookie);
 
 app.register(transactionsRoutes, { prefix: "/transactions" });
 
